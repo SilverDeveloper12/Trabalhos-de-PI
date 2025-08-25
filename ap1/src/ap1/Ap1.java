2bc497350f5e06381926036310c580f086b4ba22
SilverDeveloper12 <167832190+SilverDeveloper12@users.noreply.github.com>
2025-08-25 20:20:58 -0300

    Update Ap1.java

--- ap1/src/ap1/Ap1.java
+++ ap1/src/ap1/Ap1.java
@@ -1,14 +1,13 @@
-package ap1;
-
-
-
 //Aula 1 dos cria de API
- 
-public class Ap1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+package ap1;
+ 
+ public class Ap1 {
+
+     public static void main(String[] args) {
+         System.out.println("Hello, World!");
          
     }
     
 }
+
