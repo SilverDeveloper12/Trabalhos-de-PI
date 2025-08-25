c679c231ffe0234fb50c0745e1489abaac00e358
SilverDeveloper12 <167832190+SilverDeveloper12@users.noreply.github.com>
2025-08-25 20:14:42 -0300

    Add files via upload
    ai como eu sou bandida

--- ap1/src/ap1/Ap1.java
+++ ap1/src/ap1/Ap1.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ap1;
 
-/**
- *
- * @author enzo.ocsilva
- */
+
+
+//Aula 1 dos cria de API
+ 
 public class Ap1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello, World!");
+         
     }
     
 }
