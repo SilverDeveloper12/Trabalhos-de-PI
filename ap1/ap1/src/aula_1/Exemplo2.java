758126dd08c0885e14710da1a3b354aae81f61c8
SilverDeveloper12 <167832190+SilverDeveloper12@users.noreply.github.com>
2025-08-25 21:43:46 -0300

    Add files via upload

---
+++ ap1/ap1/src/aula_1/Exemplo2.java
@@ -0,0 +1,25 @@
+
+package aula_1;
+
+public class Exemplo2 {
+
+      public static void main(String[] args){
+        
+          //declaração de variaveis
+          
+          int n1,n2,soma;
+          
+          //entrada de dados (atribuida)
+          
+          n1 = 6;
+          n2 = 10;
+          
+          //processamento
+          
+          soma = n1 + n2;
+          
+         //Saida de dados
+          
+         System.out.println("Soma: " + soma);
+       }
+}
